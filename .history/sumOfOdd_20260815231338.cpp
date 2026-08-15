fc499b1cbec80ee44ae9e1162ccaa865398a6120
wbRayyan <i.rayanbhatti54@gmail.com>
2026-08-15 23:18:00 +0500

    Sum of All Odd numbers upto (n) using for loop

---
+++ .history/sumOfOdd_20260815231338.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i=1 , sum=0 , n;
+    cout << "Enter the number = ";
+    cin >> n;
+    
+    for(i; i<=n; i++){
+        if(i%2==0){
+            sum += i;
+        }
+    }
+
+cout << "The sum of all odd number upto " << n << " is " << sum ;
+    return 0;
+}

bd1b2b3aeb6eefdbf72a327a93bdae026e3b552d
wbRayyan <i.rayanbhatti54@gmail.com>
2026-08-16 00:07:56 +0500

    Star problem  using nested loops

---
+++ .history/nestedLoop_20260816000310.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a = '*';
+
+    for(int i=1 ; i<=5 ; i++){
+
+        for(int j=1 ; j<=5 ; j++){
+            cout << a << " "; 
+
+        }
+        cout << " " << endl;
+    }
+    return 0;
+}

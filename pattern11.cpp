2898f8b7ec8a607b69f4193884dd0b3e49c8d083
wbRayyan <i.rayanbhatti54@gmail.com>
2026-08-17 17:30:12 +0500

    Patterns through nested for loop (PYRAMID) Version

---
+++ pattern11.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter the number = ";
+    int n;
+    cin >> n;
+
+    for (int i=0 ; i<n ; i++) {
+        for(int j=0 ; j<n-i-1 ; j++){
+            cout << " ";
+        }
+
+        for(int j=1 ; j<=i+1 ; j++){
+            cout << j;
+        }
+
+        for(int j=i ; j>=1 ; j--){
+            cout << j;
+        }
+
+    cout << endl;
+         
+    }
+
+    return 0;
+}

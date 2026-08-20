e1463de8c51ca1ce2146db5b0ac9ceaad658f9ec
wbRayyan <i.rayanbhatti54@gmail.com>
2026-08-20 21:27:53 +0500

    Patterns through nested for loop (Hollow PYRAMID) Version

---
+++ pattern12.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter the number = ";
+    int n;
+    cin >> n;
+
+    // OUTER ya UPPER PART
+    for (int i=0 ; i<n ; i++) {
+        for(int j=0 ; j<n-i-1 ; j++){
+            cout << " ";
+        }
+        cout << "*";
+
+        if(i != 0){
+            for(int j=0 ; j<2*i-1 ; j++){
+                cout << " ";
+            }
+
+            cout << "*";
+        }
+
+        cout << endl;
+
+    }
+
+
+    // INNER ya LOWER PART
+    for(int i=0 ; i<n-1 ; i++){
+        for(int j=0 ; j<i+1 ; j++){
+            cout << " ";
+
+        }
+
+        cout << "*";
+
+        if(i != n-2){
+            for(int j=0 ; j<2*(n-i)-5 ; j++){
+                cout << " ";
+
+            }
+            cout << "*";
+
+        }
+        cout << endl;
+
+    }
+
+
+    return 0;
+}

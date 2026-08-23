e1746124a0aa2b771107f5aeef985c5dd0b43785
wbRayyan <i.rayanbhatti54@gmail.com>
2026-08-23 15:32:28 +0500

    Patterns through nested for loop (Forward Button) Version

---
+++ pattern15.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter the number = ";
+    int n;
+    cin >> n;
+
+    // UPER PART
+    for (int i=0 ; i<n ; i++) {
+        for(int j=0 ; j<=i ; j++){
+            cout << "* " ;
+        }
+        if(i != n-1 ){
+            for(int j=0 ; j<2*n-2-2*i ;j++){
+            cout << " ";
+            }
+        }
+        for(int j=0 ; j<=i ; j++){
+            cout << "* " ;
+        }
+        cout << endl;
+    }
+
+    //BOTTOM PART
+    for (int i=0 ; i<n ; i++) {
+
+        for(int j=0 ; j<n-1-i ; j++){
+            cout << "* " ;
+        }
+
+        if(i != n-1 ){
+            for(int j=0 ; j<2*i+2 ;j++){
+            cout << " ";
+            }
+        }
+
+        for(int j=0 ; j<n-1-i ; j++){
+            cout << "* " ;
+        }
+    
+    cout << endl;
+    }
+
+    return 0;
+}

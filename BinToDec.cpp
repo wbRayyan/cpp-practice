8104b4b48b765148611151275b8aba4bc38adfca
wbRayyan <i.rayanbhatti54@gmail.com>
2026-08-30 17:15:30 +0500

    Function for Bin To Dec conversion from 1 to N

--- BinToDec.cpp
+++ BinToDec.cpp
@@ -19,9 +19,9 @@ int main() {
     int n;
     cin >> n;
 
-    // for(int i=1 ; i<= n ; i++){
+    for(int i=1 ; i<= n ; i++){
         cout << binToDec(n) << endl;
-    // }
+    }
 
     return 0;
 } 

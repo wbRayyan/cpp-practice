bf595978b4e6c6c09170fdfa83e6315f1d1dc630
wbRayyan <i.rayanbhatti54@gmail.com>
2026-08-31 13:04:03 +0500

    Data Type Modifiers long , short , lomg long

---
+++ DataTypeModifiers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << sizeof(int) << endl;
+    cout << sizeof(long int) << endl;
+    cout << sizeof(short int) << endl;
+    cout << sizeof(long long int) << endl;
+    return 0;
+} 

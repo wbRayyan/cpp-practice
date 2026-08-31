4f44e5049c1e215867f3ff0c632c2b1de453059f
wbRayyan <i.rayanbhatti54@gmail.com>
2026-08-31 13:11:51 +0500

    Data Type Modifiers signed , unsigned

---
+++ DataTypeModifiers2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    signed int a = 10;
+    signed int b = -10;
+    unsigned int c = 10;
+    unsigned int d = -10;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    return 0;
+} 

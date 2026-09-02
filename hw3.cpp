98c1f23920b1fab6438d59cc7a9da6b1a55d8c7f
wbRayyan <i.rayanbhatti54@gmail.com>
2026-09-02 18:26:58 +0500

    Reverse of a NUM

---
+++ hw3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int reverseOfANum(int n) {
+    int rev = 0;
+    
+    while (n > 0) {
+        int rem = n % 10;        // Aakhri digit nikaala (jaise 125 mein se 5)
+        rev = (rev * 10) + rem;  // Puraane digits ko left shift kiya aur naya jora
+        n = n / 10;              // Aakhri digit remove kiya (125 ban gaya 12)
+    }
+    
+    return rev;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number you want to reverse = ";
+    cin >> n;
+    
+    cout << "The reverse of the number is "<< reverseOfANum(n) << endl;
+
+    return 0;
+}

021991285a107088dfb2b7b8b98772530cabd804
wbRayyan <i.rayanbhatti54@gmail.com>
2026-09-03 11:46:39 +0500

    Loop in  Array

---
+++ array2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks[5] = {10 ,20 ,30 ,40 , 50};
+    int size = 5;
+
+    for(int i=0 ; i<size ; i++){
+        cout << marks[i] <<endl;
+    }
+    return 0;
+}

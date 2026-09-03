383c7a9967b00ec84e3de4781490bd3c167c6ffe
wbRayyan <i.rayanbhatti54@gmail.com>
2026-09-03 11:38:21 +0500

    Intro to Array

---
+++ array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks[] = {10 ,20 ,30 ,40 , 50};
+    int student[5] = {1 , 2 , 3 , 4 , 5};
+
+    cout << student[0] << " " << marks[0] << endl;
+    cout << student[1] << " " << marks[1] << endl;
+    cout << student[2] << " " << marks[2] << endl;
+    cout << student[3] << " " << marks[3] << endl;
+    cout << student[4] << " " << marks[4] << endl;
+    cout << student[5] << " " << marks[5] << endl;
+    return 0;
+}

751a1686d8afb7ada4d573cd50ff3406dfba809b
wbRayyan <i.rayanbhatti54@gmail.com>
2026-09-07 23:36:51 +0500

    Size of Vectors

---
+++ vectors4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<char> vec = {'A', 'B', 'C', 'D', 'E', 'F'};
+
+    cout << "Size of vec = " << vec.size();
+    return 0;
+}

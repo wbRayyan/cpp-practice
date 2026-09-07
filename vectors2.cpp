8a4706ea069e9cb6055d09d760af28242371048a
wbRayyan <i.rayanbhatti54@gmail.com>
2026-09-07 23:31:32 +0500

    Another Simple Vector

---
+++ vectors2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> vec(5, 0);
+
+    cout << vec[0] << endl;
+    cout << vec[1] << endl;
+    cout << vec[2] << endl;
+    cout << vec[3] << endl;
+    cout << vec[4] << endl;
+    return 0;
+}

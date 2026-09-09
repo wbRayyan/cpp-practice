97a0f5349f78fcb04c71af5ac7cf10ee12dac5f4
wbRayyan <i.rayanbhatti54@gmail.com>
2026-09-09 16:17:55 +0500

    Real Kadanes Algo

---
+++ kadanesAlgo2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    cout << "Enter the size of array = ";
+    int size;
+    cin >> size;
+
+    int arr[size];
+
+    for(int i=0 ; i<size ; i++){
+        cout << "Enter the " << i+1 << " element of array = ";
+        cin >> arr[i];
+    }
+
+
+    int currSum = 0, maxSum = INT_MIN;
+    for(int i=0 ; i<size ; i++){
+        currSum += arr[i];
+        maxSum = max(currSum , maxSum);
+        if(currSum < 0){
+            currSum =0;
+        }
+    }
+    cout << "The max subarray sum is = " << maxSum;
+    return 0;
+}
